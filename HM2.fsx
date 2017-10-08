67f6c5696a44dc2c7ebebdc5ca257789c61323da
Rodolfo Marrero <rmarr028@fiu.edu>
2017-10-08 16:39:55 -0400

    Got #3 to work.

--- HM2.fsx
+++ HM2.fsx
@@ -1,4 +1,4 @@
-#Build a cartesian product
+//#1 - Done. Build a cartesian product
 let rec cartesian = function
   | ([], []) -> []
   | (_, []) -> []
@@ -6,8 +6,24 @@ let rec cartesian = function
   | (a::[], head::tail) -> (a, head) :: (cartesian([a], tail))
   | (x::xs, y::ys) -> (x, y) :: (cartesian([x], ys)) @ (cartesian(xs, y::ys))
 
+//#2 - Fail
+let rec powerset = function
+  | ([]) -> [[]]
+  | (x:int) -> [x]
+  | ((head : int) :: (tail : int list)) -> (powerset(head)) :: (powerset(tail))
 
-#Sample for sanity check
+//#3 - Done. Transpose a matrix of m x n
+//Requires remove_first
+let rec transpose = function
+    | [] -> []
+    | list -> (List.map (List.head) list) :: if (List.length (List.head list)) > 1 then (transpose (List.map (remove_first) list)) else [] //Just need to remove first element of each list on this recursive call
+
+//Removes the first element from any list
+let remove_first = function
+    | [] -> []
+    | x::xs -> xs
+
+//Sample for sanity check
 let rec concat = function
     | ([], []) -> []
     | (x::xs, y::ys) -> x :: y :: (concat(xs, ys))
