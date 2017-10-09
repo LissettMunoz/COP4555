7dca829b49e0608c621e78cc80079fbcaa5ecf2e
JorgeLeyvaR <jleyva@ipipeline.com>
2017-10-09 15:32:05 -0400

    Adding Ej 6

--- HM2_jorge.fsx
+++ HM2_jorge.fsx
@@ -20,9 +20,54 @@ let rec powerset =
 
 
 
+(*
+Recall that an F# function that takes two arguments can be coded 
+in either uncurried form (in which case it takes a pair as its input) or 
+curried form (in which case it takes the first argument and returns a function that takes the second argument).
+In fact it is easy to convert from one form to the other in F#. To this end, 
+define an F# function curry f that converts an uncurried function to a curried function,
+and an F# function uncurry f that does the opposite conversion. For example,
+  > (+);;
+  val it : (int -> int -> int) = <fun:it@13-7>
+  > let plus = uncurry (+);;
+  val plus : (int * int -> int)
+  > plus (2,3);;
+  val it : int = 5
+  > let cplus = curry plus;;
+  val cplus : (int -> int -> int)
+  > let plus3 = cplus 3;;
+  val plus3 : (int -> int)
+  > plus3 10;;
+  val it : int = 13
 
 
+*)
+(*
+> (+);;
+val it : (int -> int -> int) = <fun:it@3-9>
 
+> let plus= uncurry it;;
+val plus : (int * int -> int)
+
+> plus(4,5);;
+val it : int = 9
+*)
+let uncurry cFunc (a,b)= cFunc a b   
+
+
+(*
+> let cplus = curry plus;;
+val cplus : (int -> int -> int)
+
+> cplus 3;;
+val it : (int -> int) = <fun:Invoke@3253>
+
+> it 6;;
+val it : int = 9
+
+> 
+*)
+let curry uFunc a b = uFunc (a,b)
 
 
 
